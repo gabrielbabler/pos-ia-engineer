39332bfc99aee9de2017e1f375ca14fe85c75e76
Gabriel Babler <gabrielbabler@MacBook-Air-de-Gabriel-e-Leticia.local>
2026-05-12 22:37:27 -0300

    aula 06

---
+++ module-03/06-legacy-api-as-mcp/customers-mcp-template-z/src/infrastructure/customerHttpClient.ts
@@ -0,0 +1,14 @@
+import { type Customer } from "../domain/customer.ts"
+
+export class CustomerHttpClient {
+    private baseUrl: string
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl
+    }
+
+    async listCustomers(): Promise<Customer[]> {
+        const res = await fetch(`${this.baseUrl}/customers`)
+        return res.json() as Promise<Customer[]>
+    }
+    
+}
